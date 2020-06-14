964a980b6db13e839bc1ef61893f2d47b7068827
Robin A. Dorstijn <robin@gridt.org>
2020-06-14 18:18:55 +0200

    Replace catch with custom testing suite

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,28 +1,38 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-
-#include <catch.hpp>
+#include <cassert>
 #include <regex>
 #include <string> 
 
 #include "header.h"
 
-TEST_CASE( "Regex functions", "[regex]") {
+#define ASSERT_THROWS( expression )             \
+    do {                                        \
+        bool thrown = false;                    \
+        try {                                   \
+            expression;                         \ 
+        } catch (...) {     \
+            thrown = true;                      \
+        }                                       \
+        assert(!thrown);                        \
+    } while (false)                             
+
+
+void test_regex_functions() {
     tinyweb::RequestHeader *header = new tinyweb::RequestHeader();
     
     header->parse("GET http://localhost/ HTTP/1.1");
-    REQUIRE( header->get_version() == "1.1");
-    REQUIRE( header->get_method() == tinyweb::GET );
-    REQUIRE( header->get_uri() == "http://localhost/" );
+    assert( header->get_version() == "1.1");
+    assert( header->get_method() == tinyweb::GET );
+    assert( header->get_uri() == "http://localhost/" );
 
     header->parse("POST * HTTP/2.1");
-    REQUIRE( header->get_version() == "2.1");
-    REQUIRE( header->get_method() == tinyweb::POST );
-    REQUIRE( header->get_uri() == "*" );
+    assert( header->get_version() == "2.1");
+    assert( header->get_method() == tinyweb::POST );
+    assert( header->get_uri() == "*" );
 
     header->parse("CONNECT /asdqwe/qweasd/?klasmd=123 HTTP/3.3");
-    REQUIRE( header->get_version() == "3.3" );
-    REQUIRE( header->get_method() == tinyweb::CONNECT );
-    REQUIRE( header->get_uri() == "/asdqwe/qweasd/?klasmd=123" );
+    assert( header->get_version() == "3.3" );
+    assert( header->get_method() == tinyweb::CONNECT );
+    assert( header->get_uri() == "/asdqwe/qweasd/?klasmd=123" );
 
     header->parse(
         "GET /ad/asd/ HTTP/1.1\r\n"
@@ -30,17 +40,22 @@ TEST_CASE( "Regex functions", "[regex]") {
         "Other-Header: Sometext\r\n"
         "\r\n"
     );
-    REQUIRE( header->get_fields()["Content-Length"] == "500");
-    REQUIRE( header->get_fields()["Other-Header"] == "Sometext");
-    REQUIRE( header->get_fields().size() == 2);
+    assert( header->get_fields()["Content-Length"] == "500");
+    assert( header->get_fields()["Other-Header"] == "Sometext");
+    assert( header->get_fields().size() == 2);
 
-    REQUIRE_THROWS( header->parse("HTTP/") );
-    REQUIRE_THROWS( header->parse("1.1") );
-    REQUIRE_THROWS( header->parse("12.1") );
-    REQUIRE_THROWS( header->parse("HTTP/12.1") );
-    REQUIRE_THROWS( header->parse("HTTP//1.1") );
+    bool error_thrown = false;
+    ASSERT_THROWS( header->parse("HTTP/") );
+    ASSERT_THROWS( header->parse("1.1") );
+    ASSERT_THROWS( header->parse("12.1") );
+    ASSERT_THROWS( header->parse("HTTP/12.1") );
+    ASSERT_THROWS( header->parse("HTTP//1.1") );
 
-    REQUIRE_THROWS( header->parse("ASDLKM /asdasd/asd/ HTTP/1.1") );
-    REQUIRE_THROWS( header->parse("GET HTTP/1.1") );
-    REQUIRE_THROWS( header->parse("GET /aqwe/qwe?asdd=asd HTTP//1.2") );
+    ASSERT_THROWS( header->parse("ASDLKM /asdasd/asd/ HTTP/1.1") );
+    ASSERT_THROWS( header->parse("GET HTTP/1.1") );
+    ASSERT_THROWS( header->parse("GET /aqwe/qwe?asdd=asd HTTP//1.2") );
+}
+
+void main () {
+    test_regex_functions();
 }
